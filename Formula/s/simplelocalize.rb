9523e77dd32f1b88021be0daba8f0b376da47974
Jakub <jakub.pomykala@gmail.com>
2023-11-07 13:50:02 +0100

    simplelocalize 2.5.1 (new formula)

---
+++ Formula/s/simplelocalize.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class Simplelocalize < Formula
+  desc "Command-line tool for SimpleLocalize"
+  homepage "https://github.com/simplelocalize/simplelocalize-cli"
+
+  url "https://github.com/simplelocalize/simplelocalize-cli/releases/download/2.5.1/simplelocalize-cli-2.5.1.jar"
+  sha256 "0b4cdfe50272127a5be62f9e6dede13ca8e30605f3e2dd1aad7de76d12bfae58"
+
+  depends_on "openjdk@21" => :build if Hardware::CPU.arm?
+  depends_on "openjdk@21" => :build if Hardware::CPU.intel?
+  
+  def install
+    libexec.install "simplelocalize-cli-2.5.1.jar"
+    (bin/"simplelocalize").write <<~EOS
+      #!/bin/bash
+      exec java -jar "#{libexec}/simplelocalize-cli-2.5.1.jar" "$@"
+    EOS
+    chmod "+x", bin/"simplelocalize"
+  end
+
+  test do
+    output = shell_output("#{bin}/simplelocalize --version 2>&1")
+    assert_match "https://github.com/simplelocalize/simplelocalize-cli/releases", output
+    assert_match "2.5.1", output
+  end
+end
